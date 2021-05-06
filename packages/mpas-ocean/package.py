f74ae5e92831748d4537ce5b51e91a37e4a031de
Hanqi Guo <hguo@anl.gov>
2021-05-06 11:16:58 -0500

    recipes for building mpas-o

---
+++ packages/mpas-ocean/package.py
@@ -0,0 +1,50 @@
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class MpasOcean(Package):
+    """FIXME: Put a proper description of your package here."""
+
+    # FIXME: Add a proper url for your package's homepage here.
+    homepage = "https://www.example.com"
+    url      = "https://github.com/MPAS-Dev/MPAS-Model/archive/refs/tags/v6.0.tar.gz"
+
+    # FIXME: Add a list of GitHub accounts to
+    # notify when the package is updated.
+    # maintainers = ['github_user1', 'github_user2']
+
+    version('6.0', 'b5c0000be0a6bececf4426cdf946cba0727b69985478ded0011ec31282c75105')
+
+    depends_on('mpi')
+    depends_on('pio@1_7_2', type='link')
+    depends_on('netcdf-c +mpi', type='link')
+    depends_on('netcdf-fortran', type='link')
+    depends_on('parallel-netcdf', type='link')
+    depends_on('python@:2.9', type='build')
+
+    phases = ['build', 'install']
+
+    def target(self):
+        spec = self.spec
+        target = [
+                'gfortran',
+                'NETCDF={0}'.format(spec['netcdf-c'].prefix), 
+                'NETCDFF={0}'.format(spec['netcdf-fortran'].prefix),
+                'PNETCDF={0}'.format(spec['parallel-netcdf'].prefix),
+                'PIO={0}'.format(spec['pio'].prefix),
+                # 'FFLAGS={0}'.format('-mcmodel=large'),
+                'CORE=ocean'
+                ]
+        return target
+
+    def build(self, spec, prefix):
+        make(*self.target(), parallel=False)
+        mkdir('bin')
+        copy('ocean_model', 'bin')
+
+    def install(self, spec, prefix):
+        install_tree('bin', prefix.bin)
---
+++ packages/pio/package.py
@@ -0,0 +1,60 @@
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+class Pio(Package):
+    """FIXME: Put a proper description of your package here."""
+
+    homepage = "https://www.example.com"
+    url      = "https://github.com/NCAR/ParallelIO/archive/refs/tags/pio1_7_2.tar.gz"
+
+    maintainers = ['tkameyama']
+
+    version('1_7_2', 'ecc9b50e2c75f0189c76917a555aee3b6dc3cc5e38232007577c51bb2c977e39')
+
+    depends_on('mpi')
+    #depends_on('netcdf-c +mpi', type='link')
+    #depends_on('netcdf-fortran', type='link')
+    depends_on('parallel-netcdf', type='link')
+
+    phases = ['configure', 'build', 'install']
+
+    def configure_args(self):
+        spec = self.spec
+        prefix = self.prefix
+
+        config_args = [
+                '--prefix={0}'.format(prefix),
+                '--disable-netcdf',
+                '--disable-mpiio',
+                'CC=gcc',
+                'CXX=g++',
+                'FC=gfortran',
+                'F77=gfortran',
+                'F90=gfortran',
+                'MPICC=mpicc',
+                'MPICXX=mpicxx',
+                'MPIFC=mpif90',
+                'MPIF77=mpif90',
+                'MPIF90=mpif90', 
+                #'FFLAGS={0}'.format('-mcmodel=medium -fPIC'),
+                #'NETCDF_PATH=' + self.spec['netcdf-fortran'].prefix,
+                'PNETCDF_PATH=' + self.spec['parallel-netcdf'].prefix]
+
+        return config_args
+
+    def configure(self, spec, prefix):
+        with working_dir('pio'):
+            configure = Executable('./configure')
+            configure(*self.configure_args())
+
+    def build(self, spec, prefix):
+        with working_dir('pio'):
+            make(parallel = False)
+
+    def install(self, spec, prefix):
+        with working_dir('pio'):
+            make('install')
