c93f1e9fbb6209fcf01f874027dad90db3d1249d
Hanqi Guo <hguo@anl.gov>
2021-05-06 21:20:55 -0500

    libnabo

---
+++ packages/libnabo/package.py
@@ -0,0 +1,24 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class Libnabo(CMakePackage):
+    """libnabo is a fast K Nearest Neighbour library for low-dimensional spaces"""
+
+    homepage = "https://github.com/ethz-asl/libnabo"
+    url      = "https://github.com/ethz-asl/libnabo"
+    git      = "https://github.com/ethz-asl/libnabo"
+
+    version('1.0.7', tag='1.0.7')
+    version('master', branch='master')
+
+    depends_on('eigen')
+
+    #def cmake_args(self):
+    #    args = ['-DCMAKE_C_COMPILER=%s' % self.spec['mpi'].mpicc,
+    #            '-DCMAKE_CXX_COMPILER=%s' % self.spec['mpi'].mpicxx]
+    #    return args
