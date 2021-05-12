057d5bcb33f6883fb335803941910288ebda5cb1
Hanqi Guo <hguo@anl.gov>
2021-05-11 21:39:01 -0500

    decaf build mraj

--- packages/decaf/package.py
+++ packages/decaf/package.py
@@ -14,18 +14,27 @@ class Decaf(CMakePackage):
     git      = "https://github.com/tpeterka/decaf.git"
 
     version('master', branch='master')
-    version('mraj', git='https://github.com/hguo/decaf.git')
+    # version('mraj', git='https://github.com/hguo/decaf.git')
+    version('mraj', git='https://github.com/mukundraj/decaf.git', branch='dyn-lbal')
 
-    depends_on('mpi')
+
+    depends_on('mpich@3.2.1 device=ch3')
     depends_on('boost')
     depends_on('python@3:',   type='run')
-    depends_on('py-networkx', type='run')
-
-    depends_on('netcdf-c@4.6.1 +mpi', type='link', when='@mraj')
+    depends_on('py-networkx@2.0', type='run')
+    
+    depends_on('py-setuptools', type='run', when='@mraj')
+    depends_on('py-netcdf4', type='run', when='@mraj')
+    depends_on('py-scipy', type='run', when='@mraj')
+    depends_on('py-pyamg', type='run', when='@mraj')
+    depends_on('netcdf-c@4.6.1 +mpi ^hdf5@1.10.2', type='link', when='@mraj')
+    # depends_on('vtk ^mesa~llvm swr=none', type='link', when='@mraj')
     depends_on('vtk', type='link', when='@mraj')
     depends_on('parallel-netcdf@1.7.0 -shared', type='link', when='@mraj')
     depends_on('libnabo', type='link', when='@mraj')
-    depends_on('diy', type='link', when='@mraj')
+    depends_on('diy@master', type='link', when='@mraj')
+    
+    patch('patch', when='@mraj')
 
     def cmake_args(self):
         args = ['-DCMAKE_C_COMPILER=%s' % self.spec['mpi'].mpicc,
